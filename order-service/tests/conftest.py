461e12b76da3b2a01770b6a3df7b7428e1e8184f
rahari_y <rahari_y@etna-alternance.net>
2026-06-03 14:42:22 +0000

    m

--- order-service/tests/conftest.py
+++ order-service/tests/conftest.py
@@ -5,6 +5,6 @@ _fd, _path = tempfile.mkstemp(suffix=".db")
 os.environ["DB_PATH"] = _path
 os.close(_fd)
 
-from app.database import init_db  # noqa: E402
+from app.database import init_db
 
 init_db()
